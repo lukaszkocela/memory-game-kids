fb07b5d52f0751180c821709bf31a8e9b2854dcc
Łukasz Kocela <117775643+lukaszkocela@users.noreply.github.com>
2026-05-19 10:31:05 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const base = process.env.BASE_PATH || "/";
+
 export default defineConfig({
+  base,
   plugins: [react()],
   server: {
     port: 5173,
